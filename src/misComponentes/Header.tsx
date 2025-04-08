2f84bac31b8f562a0db03cb57c10b42bed6046ce
Luis López <lopezpuigluis@fpllefia.com>
2025-04-08 20:16:56 +0200

    Paginas creadas y componente header importado ( sin funcionalidad )

--- src/misComponentes/Header.tsx
+++ src/misComponentes/Header.tsx
@@ -1,27 +1,32 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 
 export default function Header() {
   return (
-    <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </>
+    <div className=" mx-auto px-4 py-6 border-b bg-red-200">
+      <div className="flex items-center justify-center p-2 ">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink className="text-3xl">Home</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink className="text-3xl">
+                Juego
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink className="text-3xl">
+                Acerca
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>{" "}
+    </div>
   );
 }
